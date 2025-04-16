87189a448c8debd6abd99204df7c701b0fb6e5db
Abdelrahman Raafat <55184522+AbdElrahman-Rafaat-Amer@users.noreply.github.com>
2025-04-16 16:47:18 +0200

    - resolve conflicts
    Took 3 minutes

--- src/main/kotlin/di/UseCasesModule.kt
+++ src/main/kotlin/di/UseCasesModule.kt
@@ -3,12 +3,10 @@ package org.example.di
 import org.example.domain.usecase.GetEasyFoodSuggestionUseCase
 import org.example.domain.usecase.GetFoodUseCase
 import org.example.domain.usecase.GuessGameUseCase
-import org.example.domain.usecase.GetFoodUC
 import org.example.domain.usecase.GetSeafoodMealsUseCase
 import org.koin.dsl.module
 
 val useCasesModule = module {
-    single { GetFoodUC(get()) }
     single { GetSeafoodMealsUseCase(get()) }
     single { GetFoodUseCase(get()) }
     single { GetEasyFoodSuggestionUseCase(get()) }
