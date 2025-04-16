e381347466d71e7035d83513626fb2542d061cdb
Abdelrahman Raafat <55184522+AbdElrahman-Rafaat-Amer@users.noreply.github.com>
2025-04-16 17:50:45 +0200

    - create GymHelperUI.kt - create GymHelperUseCase.kt - fetch all meals based on the user input calories & protein
    Took 41 minutes

--- src/main/kotlin/di/UseCasesModule.kt
+++ src/main/kotlin/di/UseCasesModule.kt
@@ -4,6 +4,7 @@ import org.example.domain.usecase.GetEasyFoodSuggestionUseCase
 import org.example.domain.usecase.GetFoodUseCase
 import org.example.domain.usecase.GuessGameUseCase
 import org.example.domain.usecase.GetSeafoodMealsUseCase
+import org.example.domain.usecase.GymHelperUseCase
 import org.koin.dsl.module
 
 val useCasesModule = module {
@@ -11,4 +12,5 @@ val useCasesModule = module {
     single { GetFoodUseCase(get()) }
     single { GetEasyFoodSuggestionUseCase(get()) }
     single { GuessGameUseCase(get()) }
+    single { GymHelperUseCase(get()) }
 }
