cfcf0864d2609d2a0e780618f318ea518b502ab3
Mohamed Elhanafy <m.elhanafy@otas.net>
2025-04-16 06:55:00 +0200

    add guess game feature with ui and use case implementations

--- src/main/kotlin/di/AppModule.kt
+++ src/main/kotlin/di/AppModule.kt
@@ -2,12 +2,9 @@ package org.example.di
 
 import org.example.data.FakeRepositoryImpl
 import org.example.domain.repository.FoodRepository
-import org.example.presentaion.GetFoodUi
 import org.koin.dsl.module
 
 val appModule = module {
     single { FakeRepositoryImpl() }
     single<FoodRepository> { FakeRepositoryImpl() }
-
-    single { GetFoodUi(get()) }
 }
--- src/main/kotlin/di/UseCasesModule.kt
+++ src/main/kotlin/di/UseCasesModule.kt
@@ -1,8 +1,10 @@
 package org.example.di
 
 import org.example.domain.usecase.GetFoodUC
+import org.example.domain.usecase.GuessGameUC
 import org.koin.dsl.module
 
 val useCasesModule = module {
     single { GetFoodUC(get()) }
+    single { GuessGameUC(get()) }
 }
