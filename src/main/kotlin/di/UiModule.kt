965db7b29ba5b4e638fb4310cc53d57be776b35f
dina othman <dinaosman581@gmail.com>
2025-04-16 16:48:29 +0200

    solve conflict

--- src/main/kotlin/di/UiModule.kt
+++ src/main/kotlin/di/UiModule.kt
@@ -3,6 +3,7 @@ package org.example.di
 import org.example.presentaion.GetEasyFoodSuggestionUi
 import org.example.presentaion.GetFoodUi
 import org.example.presentaion.GuessGameUi
+import org.example.presentaion.IraqiMealsUi
 import org.koin.dsl.module
 
 val uiModule = module {
@@ -10,4 +11,5 @@ val uiModule = module {
     single { GuessGameUi(get()) }
     single { GetFoodUi(get()) }
     single { GetEasyFoodSuggestionUi(get()) }
+    single { IraqiMealsUi(get()) }
 }
