08756d2ece651103cc2ad89ff33c1d2a017ef091
sudoku <ibrahim.magdeee@gmail.com>
2025-04-16 17:15:34 +0200

    prepare GetMealWithHighCaloriesUseCase structure

---
+++ src/main/kotlin/domain/usecase/GetMealWithHighCaloriesUseCase.kt
@@ -0,0 +1,9 @@
+package org.example.domain.usecase
+
+import org.example.domain.repository.FoodRepository
+
+
+class GetMealWithHighCaloriesUseCase(private val foodRepository: FoodRepository) {
+
+
+}
