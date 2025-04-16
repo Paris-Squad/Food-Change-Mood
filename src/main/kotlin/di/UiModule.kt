0e0cd53ab9db221cef7879e6a9c652fda56e3c2f
Abdelrahman Raafat <55184522+AbdElrahman-Rafaat-Amer@users.noreply.github.com>
2025-04-16 19:27:52 +0200

    - resolve conflict
    Took 4 minutes

--- src/main/kotlin/di/UiModule.kt
+++ src/main/kotlin/di/UiModule.kt
@@ -1,21 +1,15 @@
 package org.example.di
 
-import org.example.presentaion.GetEasyFoodSuggestionUi
-import org.example.presentaion.EggFreeSweetsUi
-import org.example.presentaion.GetFoodUi
-import org.example.presentaion.GuessGameUi
-import org.example.presentaion.GetSeafoodMealsUI
-import org.example.presentaion.IraqiMealsUi
-import org.example.presentaion.GymHelperUI
+import org.example.presentaion.*
 import org.koin.dsl.module
 
 val uiModule = module {
     single { GetFoodUi(get()) }
     single { GetSeafoodMealsUI(get()) }
     single { GuessGameUi(get()) }
-    single { GetFoodUi(get()) }
     single { GetEasyFoodSuggestionUi(get()) }
     single {IraqiMealsUi(get())  }
     single { EggFreeSweetsUi(get()) }
+    single { GetMealsByCountryUi(get()) }
     single { GymHelperUI(get()) }
 }
