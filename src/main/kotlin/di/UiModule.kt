4e38ab35e0cb85e26894d8c240eae12967279960
Abdelrahman Raafat <55184522+AbdElrahman-Rafaat-Amer@users.noreply.github.com>
2025-04-16 23:29:58 +0200

    - fix missing imports. - remove logs parsing.
    Took 13 seconds

--- src/main/kotlin/di/UiModule.kt
+++ src/main/kotlin/di/UiModule.kt
@@ -1,19 +1,16 @@
 package org.example.di
 
-import org.example.presentaion.GetEasyFoodSuggestionUi
-import org.example.presentaion.EggFreeSweetsUi
-import org.example.presentaion.GetFoodUi
-import org.example.presentaion.GuessGameUi
-import org.example.presentaion.GetSeafoodMealsUI
-import org.example.presentaion.IraqiMealsUi
+import org.example.presentaion.*
+import org.example.presentation.RandomPotatoMealsUi
 import org.koin.dsl.module
 
 val uiModule = module {
+    single { EggFreeSweetsUi(get()) }
+    single { GetEasyFoodSuggestionUi(get()) }
     single { GetFoodUi(get()) }
+    single {GetIraqiMealsUi(get())  }
+    single { GetMealsByCountryUi(get()) }
     single { GetSeafoodMealsUI(get()) }
     single { GuessGameUi(get()) }
-    single { GetFoodUi(get()) }
-    single { GetEasyFoodSuggestionUi(get()) }
-    single {IraqiMealsUi(get())  }
-    single { EggFreeSweetsUi(get()) }
+    single { RandomPotatoMealsUi(get()) }
 }
--- src/main/kotlin/di/UseCasesModule.kt
+++ src/main/kotlin/di/UseCasesModule.kt
@@ -9,15 +9,14 @@ import org.example.domain.usecase.GetSeafoodMealsUseCase
 import org.example.domain.usecase.GetMealsByCountryUseCase
 import org.koin.dsl.module
 import org.example.domain.usecase.GetRandomPotatoMealsUseCase
+
 val useCasesModule = module {
-    single { GetRandomPotatoMealsUseCase(get()) }
-    single { GetSeafoodMealsUseCase(get()) }
-    single { GetFoodUseCase(get()) }
     single { GetEasyFoodSuggestionUseCase(get()) }
-    single { GuessGameUseCase(get()) }
-    single { GetIraqiMealsUseCase(get()) }
     single { GetEggFreeSweetsUseCase(get()) }
+    single { GetFoodUseCase(get()) }
+    single { GetIraqiMealsUseCase(get()) }
     single { GetMealsByCountryUseCase(get()) }
     single { GetRandomPotatoMealsUseCase(get()) }
-
+    single { GetSeafoodMealsUseCase(get()) }
+    single { GuessGameUseCase(get()) }
 }
