a27a0a3d761385dd6f8cec7b7f5b578979abab98
aliYasser <ay6516169@gmail.com>
2025-04-17 05:24:05 +0200

    add usecase to get quick healthy food picks sorted by low fat , saturated fat and carbs

--- src/main/kotlin/di/UseCasesModule.kt
+++ src/main/kotlin/di/UseCasesModule.kt
@@ -1,14 +1,7 @@
 package org.example.di
 
-import org.example.domain.usecase.GetIraqiMealsUseCase
-import org.example.domain.usecase.GetEasyFoodSuggestionUseCase
-import org.example.domain.usecase.GetFoodUseCase
-import org.example.domain.usecase.GuessGameUseCase
-import org.example.domain.usecase.GetEggFreeSweetsUseCase
-import org.example.domain.usecase.GetSeafoodMealsUseCase
-import org.example.domain.usecase.GetMealsByCountryUseCase
+import org.example.domain.usecase.*
 import org.koin.dsl.module
-import org.example.domain.usecase.GetRandomPotatoMealsUseCase
 
 val useCasesModule = module {
     single { GetEasyFoodSuggestionUseCase(get()) }
@@ -19,4 +12,5 @@ val useCasesModule = module {
     single { GetRandomPotatoMealsUseCase(get()) }
     single { GetSeafoodMealsUseCase(get()) }
     single { GuessGameUseCase(get()) }
+    single { GetQuickHealthyPicksUseCase(get()) }
 }
