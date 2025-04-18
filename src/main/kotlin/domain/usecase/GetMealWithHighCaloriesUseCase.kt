1f49d70eb2b02a9ddc79de6d0350e3736e915f17
Ibrahim Magdi <ibrahim.magdeee@gmail.com>
2025-04-18 14:35:55 +0200

    fix conflicts with development

--- src/main/kotlin/domain/usecase/GetMealWithHighCaloriesUseCase.kt
+++ src/main/kotlin/domain/usecase/GetMealWithHighCaloriesUseCase.kt
@@ -1,24 +1,23 @@
 package org.example.domain.usecase
 
-import org.example.domain.FoodException
-import org.example.domain.repository.FoodRepository
-import org.example.model.Food
+import domain.model.Meal
+import org.example.domain.MealException
+import org.example.domain.repository.MealRepository
 
+open class GetMealWithHighCaloriesUseCase(private val mealRepository: MealRepository) {
 
-open class GetMealWithHighCaloriesUseCase(private val foodRepository: FoodRepository) {
-
-    fun invoke(requiredCalories : Float) : Result<Food> {
-      val getFoodWithHighCalories = getMealsWithRequiredCalories(requiredCalories)
+    fun invoke(requiredCalories: Float): Result<Meal> {
+        val getFoodWithHighCalories = getMealsWithRequiredCalories(requiredCalories)
 
         return if (getFoodWithHighCalories.isEmpty()) {
-            Result.failure(FoodException.NoMealWith700Calories())
+            Result.failure(MealException.NoMealsFoundException("No Meal Founded"))
         } else Result.success(getFoodWithHighCalories.random())
     }
 
-    private fun getMealsWithRequiredCalories(requiredCalories: Float) : List<Food> {
-        return foodRepository.getFood().filter { meal ->
+    private fun getMealsWithRequiredCalories(requiredCalories: Float): List<Meal> {
+        return mealRepository.getMeals().filter { meal ->
             meal.nutrition.calories == requiredCalories
-            }
+        }
     }
 }
 
--- src/main/kotlin/presentaion/GetMealWithHighCaloriesConsoleUi.kt
+++ src/main/kotlin/presentaion/GetMealWithHighCaloriesConsoleUi.kt
@@ -1,28 +1,28 @@
 package org.example.presentaion
 
-import org.example.domain.FoodException
+import org.example.domain.MealException
 import org.example.domain.usecase.GetMealWithHighCaloriesUseCase
 
 class GetMealWithHighCaloriesConsoleUi(private val useCase: GetMealWithHighCaloriesUseCase) {
 
     operator fun invoke() {
         print("Are you like this meal ? \n if you like it enter 1 or 0 if you disLike \n")
-        val userInput : Int? = readlnOrNull()?.trim()?.toIntOrNull()
+        val userInput: Int? = readlnOrNull()?.trim()?.toIntOrNull()
         userInput?.let { inputValue ->
             when (inputValue) {
-            1 -> useCase.invoke(requiredCalories = 700f).getOrNull()?.description
-            0 -> useCase.invoke(requiredCalories = 700f)
-            else ->  validateUserInput(inputValue)
+                1 -> useCase.invoke(requiredCalories = 700f).getOrNull()?.description
+                0 -> useCase.invoke(requiredCalories = 700f)
+                else -> validateUserInput(inputValue)
+            }
         }
-      }
     }
 
-  private  fun validateUserInput(isLiked : Int) {
-      try {
-          println("Invalid number. Please enter 1 or 0")
-      } catch (e: Exception) {
-          throw FoodException.InvalidInputLikedSystem()
-      }
-  }
+    private fun validateUserInput(isLiked: Int) {
+        try {
+            println("Invalid number. Please enter 1 or 0")
+        } catch (e: Exception) {
+            throw MealException.NoMealsFoundException("No Meal Founded")
+        }
+    }
 
 }
