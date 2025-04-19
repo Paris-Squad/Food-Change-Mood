8c6243d66114e1075eea9ada7606575a25bf60a7
dina othman <dinaosman581@gmail.com>
2025-04-19 10:31:55 +0200

    replace ConsoleUI with Interactor and make input validation in UseCase

--- src/main/kotlin/di/UiModule.kt
+++ src/main/kotlin/di/UiModule.kt
@@ -7,18 +7,19 @@ val uiModule = module {
     single { EggFreeSweetsConsoleUi(get()) }
     single { GetEasyMealSuggestionConsoleUi(get()) }
     single { GetMealConsoleUi(get()) }
-    single { GetIraqiMealsConsoleUi(get())  }
-    single { GetMealsByCountryConsoleUi(get()) }
+    single { GetIraqiMealsInteractor(get()) }
+    single { GetMealsByCountryInteractor(get()) }
     single { GetSeafoodMealsConsoleUi(get()) }
     single { GuessGameConsoleUi(get()) }
     single { RandomPotatoMealsConsoleUi(get()) }
     single { GymHelperConsoleUi(get()) }
-    single { SearchMealsByAddDateConsoleUi(get())}
+    single { SearchMealsByAddDateConsoleUi(get()) }
     single { GetIngredientGuessUI(get()) }
     single { GetQuickHealthyPicksConsoleUI(get()) }
     single { GetItalianLargeGroupMealsConsoleUi(get()) }
     single { GetMealWithHighCaloriesConsoleUi(get()) }
     single { KetoDietMealHelperConsoleUi(get()) }
     single { SearchByMealNameConsoleUI(get()) }
+}
 
 
