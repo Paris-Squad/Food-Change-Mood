6722becad3dc367d3f2d3f617b1608f7959414cc
Ibrahim Magdi <ibrahim.magdeee@gmail.com>
2025-04-24 09:52:55 +0200

    add HighCaloriesMeal test Case

---
+++ src/test/kotlin/domain/usecase/GetMealWithHighCaloriesUseCaseTest.kt
@@ -0,0 +1,13 @@
+package domain.usecase
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+
+class GetMealWithHighCaloriesUseCaseTest{
+
+  @BeforeEach
+  fun setup(){
+
+  }
+
+ }
