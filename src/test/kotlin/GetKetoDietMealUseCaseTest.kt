133be85cfeedf50071a120a7868767809f73de57
hossam haroon <hosamharon2016@gmail.com>
2025-04-24 12:40:17 +0200

    change the name of the function that creates fake meals. transfer the function to the test class inside companion object add a test function to test if the function throws an exception

--- src/test/kotlin/GetKetoDietMealUseCaseTest.kt
+++ src/test/kotlin/GetKetoDietMealUseCaseTest.kt
@@ -3,10 +3,13 @@ import io.mockk.every
 import io.mockk.mockk
 import org.example.domain.repository.MealRepository
 import org.example.domain.usecase.GetKetoDietMealUseCase
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import com.google.common.truth.Truth.assertThat
+import domain.model.Meal
+import domain.model.Nutrition
+import kotlinx.datetime.LocalDate
+import org.example.domain.MealException
 
 
 class GetKetoDietMealUseCaseTest{
@@ -23,9 +26,9 @@ class GetKetoDietMealUseCaseTest{
   fun `should return a random keto meal when the meal contains the required nutrition amount `(){
         // Given
         every { mealRepository.getMeals() } returns listOf(
-          createMeal(null,100f,900f,40f),
-          createMeal(500f,25f,48.3f,40f),
-          createMeal(500f,11f,400f,60f)
+            createTestMealsForKetoMealHelper(null,100f,900f,40f),
+            createTestMealsForKetoMealHelper(500f,25f,48.3f,40f),
+            createTestMealsForKetoMealHelper(500f,11f,400f,60f)
         )
 
         // When
@@ -33,7 +36,7 @@ class GetKetoDietMealUseCaseTest{
 
         // Then
       assertThat(result.getOrNull()).isEqualTo(
-          createMeal(
+          createTestMealsForKetoMealHelper(
           500f,25f,48.3f,40f
           )
       )
@@ -43,7 +46,7 @@ class GetKetoDietMealUseCaseTest{
     fun `should return a failure when one of the required nutrition is null`(){
         // Given
         every { mealRepository.getMeals() } returns listOf(
-            createMeal(500f,25f,null,40f)
+            createTestMealsForKetoMealHelper(500f,25f,null,40f)
         )
 
         // When
@@ -56,21 +59,22 @@ class GetKetoDietMealUseCaseTest{
     fun `should return only a keto meal that is not repeated`(){
         // Given
         every { mealRepository.getMeals() } returns listOf(
-            createMeal(null,100f,900f,40f),
-            createMeal(500f,25f,48.3f,42f),
-            createMeal(500f,11f,400f,60f)
+            createTestMealsForKetoMealHelper(null,100f,900f,40f),
+            createTestMealsForKetoMealHelper(500f,25f,48.3f,42f),
+            createTestMealsForKetoMealHelper(500f,11f,400f,60f)
         )
 
         // When
         val result = getKetoDietMealUseCase.getSuggestedKetoMeal(
             setOf(
-                createMeal(500f,25f,48.3f,42f) //repeated keto meal
+                createTestMealsForKetoMealHelper(500f,25f,48.3f,42f) //repeated keto meal
             )
         )
 
         // Then
         assertThat(result.isFailure).isTrue()
     }
+
     @Test
     fun `should return a failure when there is no keto meals returned from getMeals()`(){
         // Given
@@ -81,6 +85,43 @@ class GetKetoDietMealUseCaseTest{
 
         // Then
         assertThat(result.isFailure).isTrue()
+    }
+
+    @Test
+    fun `should return an exception when keto meal list is empty`(){
+        // Given
+        every{mealRepository.getMeals()} returns listOf()
+
+        // When
+        val result  = getKetoDietMealUseCase.getSuggestedKetoMeal(emptySet())
+
+        // Then
+        assertThat(result.exceptionOrNull()).isInstanceOf(MealException.NoKetoDietMealFound::class.java)
+    }
+
+    companion object{
+        fun createTestMealsForKetoMealHelper(
+            calories : Float?,
+            protein : Float?,
+            totalFat : Float?,
+            carbohydrates : Float?
+        ) = Meal(
+            mealName = null,
+            mealId = "1",
+            minutesForPreparation = 30,
+            contributorId = "1",
+            nutrition = Nutrition(
+                calories,totalFat,null,null,protein,null,carbohydrates
+            ),
+            description = null,
+            numberOfIngredients = 3,
+            numberOfSteps = 3,
+            steps = listOf("do", "do", "do"),
+            ingredients = listOf("do", "do", "do"),
+            tags = listOf("do", "do", "do"),
+            submittedDate = LocalDate(2000,12,5)
+
+        )
     }
 
 }
