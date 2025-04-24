4328abed86725a4d48f778fedb42e7f35c314270
Ibrahim Magdi <ibrahim.magdeee@gmail.com>
2025-04-24 15:05:50 +0200

    add HighCaloriesMeal use case test Case

--- src/test/kotlin/domain/usecase/GetMealWithHighCaloriesUseCaseTest.kt
+++ src/test/kotlin/domain/usecase/GetMealWithHighCaloriesUseCaseTest.kt
@@ -1,24 +1,12 @@
 package domain.usecase
 
-import io.mockk.every
-import io.mockk.mockk
-import org.example.domain.MealException
-import org.example.domain.repository.MealRepository
-import org.example.domain.usecase.GetMealWithHighCaloriesUseCase
-import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
 
 class GetMealWithHighCaloriesUseCaseTest {
-    private val mealRepository = mockk<MealRepository>(relaxed = true)
-    private lateinit var getMealWithHighCaloriesUseCase: GetMealWithHighCaloriesUseCase
 
     @BeforeEach
     fun setup() {
-        getMealWithHighCaloriesUseCase = GetMealWithHighCaloriesUseCase(mealRepository)
     }
-
 
 
 }
