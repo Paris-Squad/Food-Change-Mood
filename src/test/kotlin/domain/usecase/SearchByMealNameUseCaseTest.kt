765a576f1fe19a62f972e26fe8a2f0f60ca1191b
dina othman <dinaosman581@gmail.com>
2025-04-25 12:52:02 +0300

    add SearchByMealNameUseCaseTest

---
+++ src/test/kotlin/domain/usecase/SearchByMealNameUseCaseTest.kt
@@ -0,0 +1,85 @@
+package domain.usecase
+
+import io.mockk.mockk
+import org.example.domain.repository.MealRepository
+import org.example.domain.usecase.SearchByMealNameUseCase
+import com.google.common.truth.Truth.assertThat
+import io.mockk.every
+import org.example.domain.MealException
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import kotlin.collections.listOf
+import kotlin.test.assertEquals
+
+class SearchByMealNameUseCaseTest {
+
+    private lateinit var mealRepository: MealRepository
+    private lateinit var mealByNameUseCase: SearchByMealNameUseCase
+
+    @BeforeEach
+    fun setUp() {
+        mealRepository = mockk()
+        mealByNameUseCase = SearchByMealNameUseCase(mealRepository)
+    }
+
+    @Test
+    fun `should return meals that exactly match search term`() {
+        // Given
+        val meal1 = createMeal(mealName = "Kebab")
+        every { mealRepository.getMeals() } returns listOf(meal1)
+
+        // When
+        val result = mealByNameUseCase.invoke("Kebab")
+
+        // Then
+        assertThat(result.getOrNull()).containsExactly(meal1)
+    }
+
+    @Test
+    fun `should return meals with similar names using Levenshtein`() {
+        // Given
+        val meal1 = createMeal(mealName = "Kebab")
+        every { mealRepository.getMeals() } returns listOf(meal1)
+
+        // When
+        val result = mealByNameUseCase.invoke("Kebob")
+
+        // Then
+        assertThat(result.getOrNull()).containsExactly(meal1)
+    }
+
+    @Test
+    fun `should throw exception when no meals match`() {
+        // Given
+        val meal1 = createMeal(mealName = "Kebab")
+        every { mealRepository.getMeals() } returns listOf(meal1)
+
+        // When
+        val result = mealByNameUseCase.invoke("Pizza")
+
+        // Then
+        val exception = assertThrows<MealException.NoMealsFoundException> {
+            result.getOrThrow()
+        }
+        assertEquals("no food found matching 'Pizza'", exception.message)
+    }
+
+    @Test
+    fun `should ignore meals with null or empty names`() {
+        // Given
+        val meals = listOf(
+            createMeal(mealName = null),
+            createMeal(mealName = ""),
+            createMeal(mealName = "Kebab")
+        )
+        every { mealRepository.getMeals() } returns meals
+
+        // When
+        val result = mealByNameUseCase.invoke("Kebab")
+
+        // Then
+        assertThat(result.getOrNull()).containsExactly(meals[2])
+    }
+
+}
